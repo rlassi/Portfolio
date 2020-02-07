28ab7f7a5a13fd05500bd40759a5048a32ba353a
Ryan Lassiter <ryankylelassiter@gmail.com>
2020-02-07 12:33:32 -0500

    added updated resume file; centered resume on larger screens

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,13 +9,9 @@ const Resume = () => {
     return (
         <div className={resumeStyles.resume}>
             <div className={resumeStyles.contentContainer}>
-                <div className={resumeStyles.mobileDiv}>
                     <Document file={pdf}>
                         <Page renderMode={"svg"} pageNumber={1} />
-                        <Page renderMode={"svg"} pageNumber={2} />
                     </Document>
-                </div>
-                
             </div> 
         </div>         
     );
